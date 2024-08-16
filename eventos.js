eaee775b30275090e6c17a0eb4b7e19eace2209d
Antonella Salaberry <antonellasalaberry2701@gmail.com>
2024-08-15 21:27:07 -0300

    solución propagación de eventos

--- eventos.js
+++ eventos.js
@@ -1,4 +1,8 @@
+document.getElementById("miBoton").addEventListener("click", function(event) {
+    alert("Hola!");
+    event.stopPropagation();
+});
+
 document.getElementById("mi-div").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
-
